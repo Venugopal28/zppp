24209f5c56666f37ec1da0a64ad10322856a4931
venugopal28 <samavenu028@gmail.com>
2019-07-31 11:39:47 +0530

    july2

---
+++ july31/src/com/july31/venu12.java
@@ -0,0 +1,10 @@
+package com.july31;
+
+public class venu12 {
+
+	public static void main(String[] args) {
+		System.out.println("hai");
+
+	}
+
+}
---
+++ july31/src/com/july31/venu22.java
@@ -0,0 +1,8 @@
+package com.july31;
+
+public class venu22 {
+	public static void main(String[] args) {
+		System.out.println("bye");
+
+	}
+}
